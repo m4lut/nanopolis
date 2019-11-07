d89a8633f33a120f03d20a8e8a0e7903b5adf710
Maksim Al-Utaibi STUDENT <Maksim.Al-Utaibi@cic.ac>
2019-11-07 15:08:15 +0000

    changed printmap() to 33x2

--- Nanopolis/Game.vb
+++ Nanopolis/Game.vb
@@ -1,5 +1,5 @@
 ﻿Imports System.IO
-'0 at start menu displays 6x2 grid of various buildings, for debugging PrintMap()
+'0 at start menu displays 33x2 grid of various buildings, for debugging PrintMap()
 Module Module1
     Sub StartMenu()
         MsgBox("Welcome to Nanopolis!" & vbCrLf & "Developed by Maksim Al-Utaibi" & vbCrLf & "Make sure to maximise the console window when playing.", vbOKOnly)
@@ -39,7 +39,7 @@ Module Module1
     End Sub
     Sub PrintMap()
         Console.Clear()
-        Dim Map(,) As Integer = {{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41}, {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41}}
+        Dim Map(,) As Integer = {{-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}, {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}}
         For Row As Integer = 0 To 1
             For CurrentLine As Integer = 0 To 3
                 For Column = 0 To 41
